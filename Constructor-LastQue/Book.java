2d9e92e45209a828a04792dd4701db546e4ebf73
harshadarayate <hr215144@.com>
2025-05-07 07:03:57 +0530

    Modified

--- Constructor-LastQue/Book.java
+++ Constructor-LastQue/Book.java
@@ -1,3 +1,9 @@
+
+// 42) A) Write a Java program to create a class called "Book" with instance variables title, author, and price. Implement a default constructor and two parameterized constructors:
+// One constructor takes the title and author as parameters.
+// The other constructor takes title, author, and price as parameters.
+// Print the values of the variables for each constructor.
+
 class Book {
     String title;
     String author;
@@ -46,3 +52,47 @@ class Book {
         b3.displayDetails();
     }
 }
+
+// B) Write a Java program to create a class called "TrafficLight" with attributes for color and duration, 
+// and methods to change the color and check for red or green.
+
+// class TrafficLight {
+//     private String color;
+//     private int duration; // in seconds
+
+//     public TrafficLight(String color, int duration) {
+//         this.color = color;
+//         this.duration = duration;
+//     }
+
+//     public void changeColor(String newColor) {
+//         this.color = newColor;
+//         System.out.println("Traffic light changed to: " + color);
+//     }
+
+//     public boolean isRed() {
+//         return color.equalsIgnoreCase("red");
+//     }
+
+//     public boolean isGreen() {
+//         return color.equalsIgnoreCase("green");
+//     }
+
+//     public void displayStatus() {
+//         System.out.println("Current Color : " + color);
+//         System.out.println("Duration      : " + duration + " seconds");
+//     }
+
+//     public static void main(String[] args) {
+//         TrafficLight light = new TrafficLight("Red", 60);
+//         light.displayStatus();
+
+//         System.out.println("Is Red? " + light.isRed());
+//         System.out.println("Is Green? " + light.isGreen());
+
+//         light.changeColor("Green");
+//         light.displayStatus();
+//         System.out.println("Is Red? " + light.isRed());
+//         System.out.println("Is Green? " + light.isGreen());
+//     }
+// }
